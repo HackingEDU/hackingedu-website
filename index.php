c1dd3a12d81e242fcc869bde75e5a910eca6a91d
alex-cory <alexcoryregister@gmail.com>
2014-08-06 12:42:25 -0700

    updating about section

--- index.php
+++ index.php
@@ -75,41 +75,35 @@
 		</div><!-- /container -->
 	</div><!-- /headerwrap -->
 
-	<!-- WELCOME SECTION -->
-    <div class="container">
-      <div class="row mt">
-<!--       	<div class="col-lg-8">
-	        <h1>What is Hacking Edu?</h1>
-	        <p>This year we will have various companies from all over the bay area coming along with a couple of their trusted software engineers to help each hacker with their projects.</p>
-      	</div>
-      	<div class="col-lg-4">
-      		<p class="pull-right"><br><button type="button" class="btn btn-green">Sign Up Now!</button></p>
-      	</div> -->
-      	  <div class="col-lg-8">
-      	  	<!-- <h1>Our Process</h1> -->
-      	  	<hr>
-      	  	<h3><b>LEARN</b></h3>
-      	  	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-      	  	<br>
-      	  	<h3><b>DESIGN</b></h3>
-      	  	<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software.</p>
-      	  	<br>
-      	  	<h3><b>BUILD</b></h3>
-      	  	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-      	  	<br>
-      	  	<h3><b>EVOLVE</b></h3>
-      	  	<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software.</p>
-      	  </div><!-- col-lg-8 -->
-      	  <div class="col-lg-4">
-      		<ul class="process effect-2" id="process">
-      			<li><img src="assets/img/process/pro01.png"></li>
-      			<li><img src="assets/img/process/pro02.png"></li>
-      			<li><img src="assets/img/process/pro03.png"></li>
-      			<li><img src="assets/img/process/pro04.png"></li>
-      		</ul>
-      	  </div><!-- col-lg-4 -->
-      </div><!-- /row -->
-    </div><!-- /.container -->
+		<!-- PROCESS SECTION -->
+	    <div class="container">
+	      <div class="row mt">
+			  <div class="col-lg-8">
+			  	<h1>Our Process</h1>
+			  	<hr>
+			  	<h3><b>LEARN</b></h3>
+			  	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+			  	<br>
+			  	<h3><b>DESIGN</b></h3>
+			  	<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software.</p>
+			  	<br>
+			  	<h3><b>BUILD</b></h3>
+			  	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+			  	<br>
+			  	<h3><b>EVOLVE</b></h3>
+			  	<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software.</p>
+			  </div><!-- col-lg-8 -->
+			  <div class="col-lg-4">
+				<ul class="process effect-2" id="process">
+					<li><img src="assets/img/process/pro01.png"></li>
+					<li><img src="assets/img/process/pro02.png"></li>
+					<li><img src="assets/img/process/pro03.png"></li>
+					<li><img src="assets/img/process/pro04.png"></li>
+				</ul>
+			  </div><!-- col-lg-4 -->
+	      </div><!-- /row -->
+	    </div><!-- /.container -->
+
 
 
 	<!-- SERVICES SECTION -->
