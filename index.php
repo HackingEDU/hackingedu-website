86ea354896a01f10b1165f20b6f3619b3b5eaaa9
alex-cory <alexcoryregister@gmail.com>
2014-08-06 13:52:00 -0700

    adding favicon

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="shortcut icon" href="favicon.ico?v=1.1">
     <!-- <link rel="shortcut icon" href="hackinged.ico"> -->
     <!-- <link rel="shortcut icon" href="assets/ico/faviconΩ.png"> -->
 
