43a9bf452942cd1fdb31a0748c9a5d2c176bc8e4
alex-cory <alexcoryregister@gmail.com>
2014-08-06 13:21:09 -0700

    adding favicon

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="assets/ico/favicon.png">
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+    <!-- <link rel="shortcut icon" href="assets/ico/favicon.png"> -->
 
     <title>Hacking Edu</title>
 
