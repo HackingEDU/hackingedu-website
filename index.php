e81d13eaed33d5ff89bf2c330e87fe9e67f3385e
alex-cory <alexcoryregister@gmail.com>
2014-08-06 14:04:35 -0700

    updating social links at bottom

--- index.php
+++ index.php
@@ -444,21 +444,27 @@
 	<div id="sf">
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-4 dg">
-					<h4 class="ml">FACEBOOK</h4>
-					<p class="centered"><a href="<?php echo FACEBOOK_URL; ?>"><i class="fa fa-facebook"></i></a></p>
-					<p class="ml">> Become A Friend</p>
-				</div>
-				<div class="col-lg-4 lg">
-					<h4 class="ml">TWITTER</h4>
-					<p class="centered"><a href="<?php echo TWITTER_URL; ?>"><i class="fa fa-twitter"></i></a></p>
-					<p class="ml">> Follow Us</p>
-				</div>
-				<div class="col-lg-4 dg">
-					<h4 class="ml">GOOGLE +</h4>
-					<p class="centered"><a href="<?php echo GOOGLE_PLUS_URL; ?>"><i class="fa fa-google-plus"></i></a></p>
-					<p class="ml">> Add Us To Your Circle</p>
-				</div>
+				<a href="<?php echo FACEBOOK_URL; ?>">
+					<div class="col-lg-4 dg">
+						<h4 class="ml">FACEBOOK</h4>
+						<p class="centered"><i class="fa fa-facebook"></i></p>
+						<p class="ml">> Become A Friend</p>
+					</div>
+				</a>
+				<a href="<?php echo TWITTER_URL; ?>">
+					<div class="col-lg-4 lg">
+						<h4 class="ml">TWITTER</h4>
+						<p class="centered"><i class="fa fa-twitter"></i></p>
+						<p class="ml">> Follow Us</p>
+					</div>
+				</a>
+				<a href="<?php echo GOOGLE_PLUS_URL; ?>">
+					<div class="col-lg-4 dg">
+						<h4 class="ml">GOOGLE +</h4>
+						<p class="centered"><i class="fa fa-google-plus"></i></p>
+						<p class="ml">> Add Us To Your Circle</p>
+					</div>
+				</a>
 			</div><!-- row -->
 		</div><!-- container -->
 	</div><!-- Social Footer -->
