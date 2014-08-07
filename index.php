0171432eff01e5bfd67cdbf40f56fd0dcee7db3d
alex-cory <alexcoryregister@gmail.com>
2014-08-07 02:07:33 -0700

    refactoring: added application directory and the sub directories. i.e. controllers, models, libraries, etc.

--- index.php
+++ index.php
@@ -1,4 +1,64 @@
-<?php include_once 'views/header.php'; ?>
+<?php // include_once 'views/header.php'; ?>
+<?php require_once './application/config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="favicon.ico?v=1.1">
+    <!-- <link rel="shortcut icon" href="hackinged.ico"> -->
+    <!-- <link rel="shortcut icon" href="assets/ico/faviconΩ.png"> -->
+
+    <title>Hacking Edu</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+    <script src="assets/js/modernizr.custom.js"></script>
+
+  </head>
+
+  <body>
+
+	<!-- Menu -->
+	<nav class="menu " id="theMenu">
+		<div class="menu-wrap">
+			<h1 class="logo"><a href="#home">Hacking Edu</a></h1>
+			<i class="fa fa-arrow-right menu-close"></i>
+			<a href="#home">Home</a>
+			<a href="#about">About</a>
+			<a href="#sponsors">Sponsors</a>
+			<a href="#rules">Rules</a>
+			<a href="#schedule">Schedule</a>
+			<!-- <a href="#myModal">Contact</a> -->
+			<a class="" data-toggle="modal" data-target="#myModal">Contact</a>
+			<a href="<?php echo FACEBOOK_URL; ?>"><i class="fa fa-facebook"></i></a>
+			<a href="<?php echo TWITTER_URL; ?>"><i class="fa fa-twitter"></i></a>
+			<a href="<?php echo GOOGLE_PLUS_URL; ?>" rel="publisher"><i class="fa fa-google-plus"></i></a>
+			<a href="mailto:hackingeduteam@gmail.com"><i class="fa fa-envelope"></i></a>
+		</div>
+
+		<!-- Menu button -->
+		<div id="menuToggle"><i class="fa fa-bars"></i></div>
+	</nav>
+
 
 	<!-- MAIN IMAGE SECTION -->
 	<div id="home" class="headerwrap ">
