b6e95841f0df4bc912e89c636de9c86f19364016
alex-cory <alexcoryregister@gmail.com>
2014-08-06 23:55:37 -0700

    updating rules

--- index.php
+++ index.php
@@ -340,8 +340,8 @@
 				        </fieldset>
 				      </form>
 				    </div>
-				    <div class="clearfix"></div>
-				  </div>
+				    <!-- <div class="clearfix"></div> -->
+				</div>
 			</div><!-- row -->
 		</div><!-- container -->
 	</div><!-- services section -->
@@ -386,29 +386,34 @@
 				<div class="row mt">
 			       	<div class="col-lg-8 white-text">
 			 	        <h1 style="color: white">Hackathon Rules</h1>
-                        <div class="col-lg-offset-2">
-			 	        	<h3>Spot Free Theme</h3>
-			 	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-			 	        	<h3>IN A NUTSHELL...</h3>
-			 	        	<p>Use common sense, and listen to HackIllinois staff. Let us know if you have any questions!</p>
-			 	        	<h3>GENERAL</h3>
-							<p>Rules are subject to change at any time without notice. Decisions by the HackIllinois team are final. Be sure to read our code of conduct.</p>
-			 	        	<h3>PARTICIPATION</h3>
-							<p>All participants must be full-time or part-time University students during Spring 2014 except where permission is explicitly given. (We’re willing to work with high school students — contact us). Participation is closed to family members of judges. Upon participation in this event, it is implied that we reserve the right to share your resume and other application material with our corporate sponsors.</p>
-			 	        	<h3>TEAMS</h3>
-							<p>Teams must consist of between 1 and 4 participants. Teams must submit a list of members upon submitting their hack by Sunday April 13th, 2014 at 10:00 am. Any modifications after that must be approved by organizers. You can, by all means go solo, but we encourage you to work in groups!</p>
-			 	        	<h3>SUBMISSIONS</h3>
-							<p>Each team is permitted one submission that must be received by 10:00am on Sunday, April 13th, 2014. Code submitted must be produced only by team members and written during HackIllinois from the starting time on Friday, April 11th, 2014 to the submission time. If you are continuing a previous project or using old code written by you, let us know on the project submission form. Third party libraries are allowed without notice provided that they are publicly available to any participant. All code you create at HackIllinois is owned by you, and may be made open source at your discretion. By submitting a project, you and your team grant the HackIllinois team the unrestricted right to use your submission material, at no cost, for promotional purposes of future HackIllinois events.</p>
-			 	        	<h3>JUDGING CRITERIA</h3>
-							<p>Teams’ demos will be judged on several criteria including usefulness, technical difficulty, polish, and creativity. More details about the judging process will be provided closer to the event. Teams will not be told their scores during or after judging, and decisions by the judges and HackIllinois team are final.</p>
-			 	        	<h3>PROJECT FAIR</h3>
-							<p>At the end of the event, teams will showcase their projects in a project fair. During this time period, judges will proceed to all teams, and a team will have one to two minutes to demo their projects in a project fair. A team’s presentation may be interrupted by a judge at any time. Teams must be ready to demo when the judges reach their table, and passing on a judge relinquishes the right to be judged. Corporate prizes are judged directly by the company, and HackIllinois is not responsible for the criteria used by these companies.</p>
-			 	        	<h3>WINNERS</h3>
-							<p>Winners for general HackIllinois prizes will be selected by a combination of corporate judges and judges provided by HackIllinois. Each winning team will be responsible for working with the University of Illinois Computer Science department to arrange for the payment to be sent. Corporate prizes are judged by the respective company, and are provided directly from a company to the winning team. The winning team is responsible for all taxes associated with a prize.</p>
-			 	        	<h3>PHOTOS</h3>
-							<p>By attending HackIllinois, you acknowledge that your picture and video may be used without compensation, prior permission, or notification in any promotional material related to HackIllinois.</p>
-			 	        	<h3>DID WE MISS SOMETHING?</h3>
-							<p>Have a question, or think there's something we should have included here, but didn’t? Drop us a line.</p>
+			 	        <div class="auth2">
+			 	            <!-- <h1 class="text-center">Bootstrap form theme <span>It's nice!</span> </h1> -->
+			 	            <div id="big-form" class="well auth-box2" style="color: #fff">
+	                        <!-- <div class="col-lg-offset-2"> -->
+				 	        	<h3>Spot Free Theme</h3>
+				 	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+				 	        	<h3>IN A NUTSHELL...</h3>
+				 	        	<p>Use common sense, and listen to HackIllinois staff. Let us know if you have any questions!</p>
+				 	        	<h3>GENERAL</h3>
+								<p>Rules are subject to change at any time without notice. Decisions by the HackIllinois team are final. Be sure to read our code of conduct.</p>
+				 	        	<h3>PARTICIPATION</h3>
+								<p>All participants must be full-time or part-time University students during Spring 2014 except where permission is explicitly given. (We’re willing to work with high school students — contact us). Participation is closed to family members of judges. Upon participation in this event, it is implied that we reserve the right to share your resume and other application material with our corporate sponsors.</p>
+				 	        	<h3>TEAMS</h3>
+								<p>Teams must consist of between 1 and 4 participants. Teams must submit a list of members upon submitting their hack by Sunday April 13th, 2014 at 10:00 am. Any modifications after that must be approved by organizers. You can, by all means go solo, but we encourage you to work in groups!</p>
+				 	        	<h3>SUBMISSIONS</h3>
+								<p>Each team is permitted one submission that must be received by 10:00am on Sunday, April 13th, 2014. Code submitted must be produced only by team members and written during HackIllinois from the starting time on Friday, April 11th, 2014 to the submission time. If you are continuing a previous project or using old code written by you, let us know on the project submission form. Third party libraries are allowed without notice provided that they are publicly available to any participant. All code you create at HackIllinois is owned by you, and may be made open source at your discretion. By submitting a project, you and your team grant the HackIllinois team the unrestricted right to use your submission material, at no cost, for promotional purposes of future HackIllinois events.</p>
+				 	        	<h3>JUDGING CRITERIA</h3>
+								<p>Teams’ demos will be judged on several criteria including usefulness, technical difficulty, polish, and creativity. More details about the judging process will be provided closer to the event. Teams will not be told their scores during or after judging, and decisions by the judges and HackIllinois team are final.</p>
+				 	        	<h3>PROJECT FAIR</h3>
+								<p>At the end of the event, teams will showcase their projects in a project fair. During this time period, judges will proceed to all teams, and a team will have one to two minutes to demo their projects in a project fair. A team’s presentation may be interrupted by a judge at any time. Teams must be ready to demo when the judges reach their table, and passing on a judge relinquishes the right to be judged. Corporate prizes are judged directly by the company, and HackIllinois is not responsible for the criteria used by these companies.</p>
+				 	        	<h3>WINNERS</h3>
+								<p>Winners for general HackIllinois prizes will be selected by a combination of corporate judges and judges provided by HackIllinois. Each winning team will be responsible for working with the University of Illinois Computer Science department to arrange for the payment to be sent. Corporate prizes are judged by the respective company, and are provided directly from a company to the winning team. The winning team is responsible for all taxes associated with a prize.</p>
+				 	        	<h3>PHOTOS</h3>
+								<p>By attending HackIllinois, you acknowledge that your picture and video may be used without compensation, prior permission, or notification in any promotional material related to HackIllinois.</p>
+				 	        	<h3>DID WE MISS SOMETHING?</h3>
+								<p>Have a question, or think there's something we should have included here, but didn’t? Drop us a line.</p>
+								</div>
+							</div>
 			       		</div>
 		       		</div>
 		       	</div>
