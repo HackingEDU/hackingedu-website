4de63a0e2d8c4c17a3da95930617623a0ba7be5f
alex-cory <alexcoryregister@gmail.com>
2014-08-22 12:12:58 -0700

    updated about section with content

--- views/about.php
+++ views/about.php
@@ -2,30 +2,30 @@
       <div class="row mt">
       	<div id="about" class="col-lg-8">
 	        <h1>What is Hacking Edu?</h1>
-	        <p>This year we will have various companies from all over the bay area coming along with a couple of their trusted software engineers to help each hacker with their projects.</p>
+	        <p>HackingEDU is helping students who are fed up with the way the education system is today to innovate new ways to improve it.  We want you and your team to bring a product to market to make a difference!</p>
       	</div>
-      	<div class="col-lg-4">
+      	<div class="col-lg-4 hide-on-mobile">
       		<span class="pull-right"><br><a class="apply-btn" href="#apply"><button type="button" class="btn btn-green">Sign Up Now!</button></a></span>
       	</div>
       	  <div class="col-lg-8">
       	  	<!-- <h1>Our Process</h1> -->
       	  	<hr>
       	  	<h3><b>LEARN</b></h3>
-      	  	<p>We want the students to not only learn, but to show the world what new educational ebolutions can be created to push our current system forward.  To inspire a new kind of educational system that works more effectively than the current one.</p>
+      	  	<p>We want the students to not only learn, but to show the world what new educational evolutions can be created to push our current system forward.  To inspire a new kind of educational system that works more effectively than the current one.</p>
       	  	<br>
-      	  	<h3><b>DESIGN</b></h3>
+      	  	<!-- <h3><b>DESIGN</b></h3>
       	  	<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software.</p>
-      	  	<br>
+      	  	<br> -->
       	  	<h3><b>BUILD</b></h3>
-      	  	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      	  	<p>It's time to show what you've got!  They say ideas are great, but execution is where people fall short.  Lets see what you can do in the 36 hours provided.</p>
       	  	<br>
       	  	<h3><b>EVOLVE</b></h3>
-      	  	<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software.</p>
+      	  	<p>We believe that the current education system is outdated.  If we are on the same wavelength then lets make a change.  Let us evolve the way we think about education.</p>
       	  </div><!-- col-lg-8 -->
       	  <div class="col-lg-4">
-      		<ul class="process effect-2" id="process">
+      		<ul class="process effect-2 hide-on-mobile" id="process">
       			<li><img src="assets/img/process/pro01.png"></li>
-      			<li><img src="assets/img/process/pro02.png"></li>
+      			<!-- <li><img src="assets/img/process/pro02.png"></li> -->
       			<li><img src="assets/img/process/pro03.png"></li>
       			<li><img src="assets/img/process/pro04.png"></li>
       		</ul>
