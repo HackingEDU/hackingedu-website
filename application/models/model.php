875f1fc195f4203fb4f6297546eb44294864111d
alex-cory <alexcoryregister@gmail.com>
2014-09-11 20:56:36 -0700

    debugging

--- application/models/model.php
+++ application/models/model.php
@@ -1,15 +1,17 @@
 <?php
 echo "<h2>Our website is sick and has a cold :(</h2><br> Don't worry though! We've found the doctor and are working hard to nurse her back to health!<br><br><h3>Debugging</h3>";
 
-// $commands[] = 'cd ../ && pwd';
-// $commands[] = 'ls .';
-// $commands[] = 'ls ./';
-// $commands[] = 'ls ../';
-// $commands[] = 'pwd';
 
-// foreach ($commands as $command) {
-// 	echo "command: " . $command . " = " . exec($command) . '<br>';
-// }
+$commands[] = 'cd ../hackingedu/libraries && pwd';
+$commands[] = 'cd ../ && pwd';
+$commands[] = 'ls .';
+$commands[] = 'ls ./';
+$commands[] = 'ls ../';
+$commands[] = 'pwd';
+
+foreach ($commands as $command) {
+	echo "command: " . $command . " = " . exec($command) . '<br>';
+}
 
 // if (file_exists('/DELETEme.php')) {
 // 	die('/DELETEme.php');
