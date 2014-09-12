9de071351594a241b66180f6b175b36a6391f8ec
alex-cory <alexcoryregister@gmail.com>
2014-09-11 21:47:55 -0700

    debugging

--- application/models/model.php
+++ application/models/model.php
@@ -1,31 +1,29 @@
 <?php
-echo "<h2>Our website is sick and has a cold :(</h2><br> Don't worry though! We've found the doctor and are working hard to nurse her back to health!<br><br><h3>Debugging</h3>";
-// --------------------------------------------------------------------
 
-$commands[] = 'cd ../hackingedu/libraries && pwd';
-$commands[] = 'cd ../ && pwd';
-$commands[] = 'ls .';
-$commands[] = 'ls ./';
-$commands[] = 'ls ../';
-$commands[] = 'pwd';
-
-foreach ($commands as $command) {
-	echo "command: " . $command . " = " . exec($command) . '<br>';
-}
-
-
-
-// die(get_include_path() . PATH_SEPARATOR);
-
-$google_api_php_client = './hackingedu/libraries/google-api-php-client/src/Google/Client.php';
+$google_api_php_client = './libraries/google-api-php-client/src/Google/Client.php';
 if (file_exists('./application/config.php')) { require_once './application/config.php'; } else { echo "You're missing your config file! :("; }
 if (file_exists($google_api_php_client)) {
 	set_include_path(get_include_path() . PATH_SEPARATOR . './libraries/google-api-php-client/src'); # To get Access Tokens to work
 	include_once $google_api_php_client;
 } else {
+	echo "<h2>Our website is sick and has a cold :(</h2><br> Don't worry though! We've found the doctor and are working hard to nurse her back to health!<br><br><h3>Debugging</h3>";
+	// --------------------------------------------------------------------
+
+	$commands[] = 'cd ../hackingedu/libraries && pwd';
+	$commands[] = 'cd ../ && pwd';
+	$commands[] = 'ls .';
+	$commands[] = 'ls ./';
+	$commands[] = 'ls ../';
+	$commands[] = 'pwd';
+
+	foreach ($commands as $command) {
+		echo "command: " . $command . " = " . exec($command) . '<br>';
+	}
+
 	echo "<h4>your library files aparently aren't getting included</h4>";
 	set_include_path(get_include_path() . PATH_SEPARATOR . './libraries/google-api-php-client/src'); # To get Access Tokens to work
 	include_once $google_api_php_client;
+	// die(get_include_path() . PATH_SEPARATOR);
 }
 
 
@@ -49,8 +47,6 @@ class Model
 
 			require_once './libraries/php-google-spreadsheet-client-master/src/Google/Spreadsheet/DefaultServiceRequest.php';
 			$this->client = new Google_Client();
-			var_dump($this->client);
-			die('after instantiating new Google_Client inside model');
 			$this->client->setApplicationName("HackingEDU");
 			$this->client->setClientId(CLIENT_ID);
 
