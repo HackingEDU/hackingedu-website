e6bf1f4aa732f05fe68a611fe33861832b2a4937
alex-cory <alexcoryregister@gmail.com>
2014-09-13 21:30:40 -0700

    fixed google+ links

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,54 @@
     });
 })(jQuery);
 
+// Coming Soon
+(function($) {
+    /**
+    * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
+    * according to the GMT+0 Timezone
+    **/
+    var launch = new Date(2015, 06, 14, 11, 00);
+    /**
+    * The script
+    **/
+    var message = $('#message');
+    var days = $('#days');
+    var hours = $('#hours');
+    var minutes = $('#minutes');
+    var seconds = $('#seconds');
+
+    setDate();
+    function setDate(){
+        var now = new Date();
+        if( launch < now ){
+            days.html('<h1>0</H1><p>Day</p>');
+            hours.html('<h1>0</h1><p>Hour</p>');
+            minutes.html('<h1>0</h1><p>Minute</p>');
+            seconds.html('<h1>0</h1><p>Second</p>');
+            message.html('OUR SITE IS NOT READY YET...');
+        }
+        else{
+            var s = -now.getTimezoneOffset()*60 + (launch.getTime() - now.getTime())/1000;
+            var d = Math.floor(s/86400);
+            days.html('<h1>'+d+'</h1><p>Day'+(d>1?'s':''),'</p>');
+            s -= d*86400;
+
+            var h = Math.floor(s/3600);
+            hours.html('<h1>'+h+'</h1><p>Hour'+(h>1?'s':''),'</p>');
+            s -= h*3600;
+
+            var m = Math.floor(s/60);
+            minutes.html('<h1>'+m+'</h1><p>Minute'+(m>1?'s':''),'</p>');
+
+            s = Math.floor(s-m*60);
+            seconds.html('<h1>'+s+'</h1><p>Second'+(s>1?'s':''),'</p>');
+            setTimeout(setDate, 1000);
+
+            message.html('OUR SITE IS NOT READY YET, BUT WE ARE COMING SOON');
+        }
+    }
+})(jQuery);
+
 // Animate On Scroll Feature
 (function(e, t, n, r) {
     function o(t, n) {
--- views/schedule.php
+++ views/schedule.php
@@ -8,6 +8,14 @@
 				<ul class="process effect-2" id="process">
 					<li>
 						<h1 class="coming-soon">Coming Soon!</h1>
+						<!-- START TIMER -->
+						<div id="timer" data-animated="FadeIn">
+						    <p id="message"></p>
+						    <div id="days" class="timer_box"></div>
+						    <div id="hours" class="timer_box"></div>
+						    <div id="minutes" class="timer_box"></div>
+						    <div id="seconds" class="timer_box"></div>
+						</div>
 						<!-- <img src="assets/img/sp/p01.png"> --></li>
 					<!-- <li><img src="assets/img/sp/p02.png"></li> -->
 <!-- 					<li><img src="assets/img/sp/p04.png"></li>
