b98387950e12ebc48c33d75ab3a0811b211640f6
James Kao <james@thekao.com>
2014-11-11 17:10:23 -0800

    google+edsurge logos

--- views/sponsors.php
+++ views/sponsors.php
@@ -1,11 +1,24 @@
 		<div id="sponsors">
-<!-- 			<div class="container ">
+
+ 			<div class="container">
 				<div class="row">
 					<div class="col-lg-12">
 						<h1 class="pull-left" style="color: white">2015 Hacking Edu Sponsors</h1>
 					</div>
 				</div>
 			</div>
+
+			<div class="container">
+				<div class="row level-1">
+					<div class="col-md-6">
+						<a href="https://www.google.com/"><img alt="Google" src="../img/google-logo.png"></a>
+					</div>
+					<div class="col-md-6">
+						<a href="https://www.edsurge.com/"><img alt="edSurge" src="../img/edsurge-logo.png"></a>
+					</div>
+				</div>
+			</div>
+
 			<!-- BOOTSTRAP SLIDER (it sucks...) -->
 			<!-- <div class="container"> -->
 			  <!-- <div class="row">
