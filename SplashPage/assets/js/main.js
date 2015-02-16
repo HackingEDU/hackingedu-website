f025be095c858d6dbcf2c35c635801511b608412
James Kao <james@thekao.com>
2015-02-15 16:36:36 -0800

    rm repo dependency

---
+++ SplashPage/assets/js/main.js
@@ -0,0 +1,21 @@
+/*
+* This is the plugin
+*/
+(function(a){a.createModal=function(b){defaults={title:"",message:"Your Message Goes Here!",closeButton:true,scrollable:false};var b=a.extend({},defaults,b);var c=(b.scrollable===true)?'style="max-height: 420px;overflow-y: auto;"':"";html='<div class="modal fade" id="myModal">';html+='<div class="modal-dialog">';html+='<div class="modal-content">';html+='<div class="modal-header">';html+='<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';if(b.title.length>0){html+='<h4 class="modal-title">'+b.title+"</h4>"}html+="</div>";html+='<div class="modal-body" '+c+">";html+=b.message;html+="</div>";html+='<div class="modal-footer">';if(b.closeButton===true){html+='<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'}html+="</div>";html+="</div>";html+="</div>";html+="</div>";a("body").prepend(html);a("#myModal").modal().on("hidden.bs.modal",function(){a(this).remove()})}})(jQuery);
+
+/*
+* Here is how you use it
+*/
+$(function(){    
+    $('.view-pdf').on('click',function(){
+        var pdf_link = $(this).attr('href');
+        var iframe = '<div class="iframe-container"><iframe src="'+pdf_link+'"></iframe></div>'
+        $.createModal({
+        title:'My Title',
+        message: iframe,
+        closeButton:true,
+        scrollable:false
+        });
+        return false;        
+    });    
+})
---
+++ SplashPage/sponsor_RohitVersion.php
@@ -0,0 +1,455 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>HackingEdu Sponsorship</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="assets/css/normalize.css">
+  <link rel="stylesheet" href="assets/css/skeleton.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="">
+
+</head>
+<body>
+
+<body style="background-image:url('assets/img/colorful.jpg');">
+
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<h1 class="text-left"> Overview </h1>
+
+<p class="text-left">Many people are frustrated with our current education system, and HackingEDU is empowering students to step 
+
+up and make a difference. This fall, over 1000 hackers will battle for 36 hours to create revolutionary products 
+
+that will redefine the education system. We are providing the launchpad for students to invent the future of 
+
+education and reinstate a new code for education practices.</p>
+
+<br>
+
+  <h1 class="text-left"> Why We Are Different </h1>
+
+<p class="text-left">HackingEDU was brought to life when a group of 11 Google Student Ambassadors were asked to hold small
+
+events at each of their respective universities, but instead, they dreamed of something much bigger. We are 
+
+bringing together students from all across the west coast and inspiring them to change the world. At most 
+
+hackathons, students have trouble coming up with meaningful ideas. At HackingEDU, students come because 
+
+they want to revolutionize education. This massive collaboration of ideas and implementation will bring about 
+
+change to the education system as a whole. 
+
+<br>
+
+<br>
+Our team has a proven track record, having coordinated other hackathons such as Citrus Hack, HackSC, Davis 
+
+Hacks, and Cal Hacks. ​We have a dedicated core team that spans over 11 universities and more than 20 ​other key 
+
+members, so you can be confident that the hackathon will run smoothly and solidify its place as one of the most 
+
+elite hackathons in the nation.</p>
+
+<br>
+
+  <h1 class="text-left"> Benefits of Sponsoring </h1>
+
+<p class="text-left">HackingEDU provides a great opportunity for your company to be highlighted at the national stage, establish
+
+sustainable connections with multiple universities at once, and tap into the pool of up-and coming tech talent.</p>
+
+<br>
+
+  <h1 class="text-left"> As a Sponsor You Will Be Able to: </h1>
+
+<p class="text-left"><ul>
+  <li  class="text-left">Recruit from the top collegiate developers out of Silicon Valley and across the entire West Coast</li>
+    <li class="text-left"> Demo your APIs and tools to developers and discover innovative ways in which they are used</li>
+  <li class="text-left">Promote your brand and technology to thousands of tech enthusiasts and influencers</li>
+  <li class="text-left">Mentor and assist developers with their hacking projects</li>
+    <li class="text-left">Integrate your products into the next generation of programmers’ creations</li>
+  <li class="text-left">Get in-depth feedback on developers’ skills & interest levels at HackingEDU Training Day</li>
+  <li class="text-left">Support the open source community</li>
+
+
+</ul></p>
+
+
+
+
+  <h1 class="text-left"> Sponsorship Benefits </h1>
+  <br>
+
+<table  class="text-left" class="u-full-width">
+  <thead>
+    <tr>
+      <th><h2>General</h2></th>
+      <th>Freshman $2.5K</th>
+      <th>Sophomore $7.5K</th>
+      <th>Junior $12K</th>
+      <th>Senior $20K</th>
+      <th>Masters $35K</th>
+      <th>Doctorate $50K</th>
+
+      
+
+
+    </tr>
+
+    
+  </thead>
+  <tbody>
+    <tr>
+      <td>Mentors/Engineers at HackingEDU</td>
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+</td>
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+
+    </tr>
+    <tr>
+  <td>Reserved Company Table</td>
+      <td class="yes"></td>
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+    </tr>
+ <tr>
+  <td>Judging Spot for HackingEDU Prizes</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+    </tr>
+
+     <tr>
+  <td>Custom Branded Prize</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes">$1000</td>
+      <td class="yes">$2000</td>
+      <td class="yes">$3500</td>
+      <td class="yes">$4000</td>
+    </tr>
+
+     <tr>
+  <td>Tech Talk during the Hackathon</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes">30 minutes</td>
+      <td class="yes">1 hour</td>
+      <td class="yes">Keynote</td>
+      <td class="yes">Keynote</td>
+    </tr>
+
+     <tr>
+  <td>Pre-hackathon API Demo</td>
+      <td class="yes"></td>
+       <td class="yes"></td>
+      <td class="yes"></td>
+
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+    </tr>
+
+     <tr>
+  <td>Sponsored Meal</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+    </tr>
+
+     <tr>
+  <td>Can add perks (office visit, etc.) to
+
+top prizes</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+    </tr>
+
+     <tr>
+  <td>Talk at Opening Ceremony</td>
+        <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+
+      <td class="yes">5 minutes</td>
+      <td class="yes">10 minutes</td>
+    </tr>
+
+     <tr>
+  <td>Talk at Closing Ceremony</td>
+       <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+
+      <td class="yes"> 3 minutes</td>
+      <td class="yes">5 minutes</td>
+    </tr>
+
+     <tr>
+  <td>2 Custom Questions on Signup Form</td>
+       <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+    </tr>
+
+
+
+  </tbody>
+</table>
+
+
+
+<!-- Recruiting below. New Table -->
+
+<table  class="text-left" class="u-full-width">
+  <thead>
+    <tr>
+      <th><h2>Recruiting</h2></th>
+      <th>Freshman $2.5K</th>
+      <th>Sophomore $7.5K</th>
+      <th>Junior $12K</th>
+      <th>Senior $20K</th>
+      <th>Masters $35K</th>
+      <th>Doctorate $50K</th>
+
+      
+
+
+    </tr>
+
+    
+  </thead>
+  <tbody>
+    <tr>
+      <td>Recruiters at HackingEDU</td>
+      <td class="yes">1</td>
+      <td class="yes">2</td>
+      <td class="yes">3</td>
+      <td class="yes">5</td>
+      <td class="yes">8</td>
+      <td class="yes">12</td>
+
+    </tr>
+    <tr>
+  <td>Access to attendee profiles after event</td>
+      <td class ="yes"></td>
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+    </tr>
+ <tr>
+  <td>Early access to participant info</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+
+    </tr>
+
+     <tr>
+  <td>Private interview location</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes">4 hours</td>
+      <td class="yes">Unlimited</td>
+      <td class="yes">Unlimited</td>
+    </tr>
+
+
+
+  </tbody>
+</table>
+
+
+
+<!-- Branding below. New Table -->
+
+<table class="text-left" class="u-full-width">
+  <thead>
+    <tr>
+      <th><h2>Branding</h2></th>
+      <th>Freshman $2.5K</th>
+      <th>Sophomore $7.5K</th>
+      <th>Junior $12K</th>
+      <th>Senior $20K</th>
+      <th>Masters $35K</th>
+      <th>Doctorate $50K</th>
+
+      
+
+
+    </tr>
+
+    
+  </thead>
+  <tbody>
+    <tr>
+      <td>Thanked at kickoff demos</td>
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+
+
+
+    </tr>
+    <tr>
+  <td>Logo Placement</td>
+      <td class="yes">Website</td>
+      <td class="yes">Website,T-Shirt</td>
+      <td class="yes">Website,T-Shirt,
+      <br> Swag</td>
+      <td class="yes">Website,T-Shirt,<br> Swag</td>
+      <td class="yes">Website,T-Shirt, <br>Swag, Social Media</td>
+      <td class="yes">Website,T-Shirt, <br>Swag, Social Media</td>
+
+    </tr>
+ <tr>
+  <td>Can distribute company swag</td>
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+
+    </tr>
+
+     <tr>
+  <td>Placement of company banners</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+
+    </tr>
+
+
+
+  <td>Swag in swag bags</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+
+    </tr>
+
+     <tr>
+  <td>Sponsored activities (raffles, etc.)</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+    </tr>
+     <tr>
+
+
+
+     <tr>
+  <td>Pre-Hackathon Social Media Post</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+    </tr>
+     <tr>
+  <td>Company branded email</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes">1</td>
+      <td class="yes">2</td>
+
+    </tr>
+     <tr>
+  <td>Mobile/push notifications during event</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes">1</td>
+    </tr>
+
+  <td>  HackingEDU Co-Hosted By You</td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+      <td class="yes"></td>
+        <td class="yes"> <img class = "center" src="assets/img/checkmark-icon.png" alt="" style="width:25px;height:25px">
+    </tr>
+
+
+
+  </tbody>
+</table>
+
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
