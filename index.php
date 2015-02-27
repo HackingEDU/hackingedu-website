af6f4bef08955fd473b5cdbe53d7f765d2d3b544
James Kao <james@thekao.com>
2015-02-27 11:06:46 -0800

    largest education hackathon

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta property="og:url" content="http://hackingedu.co"/>
     <meta property="og:type" content="website"/>
     <meta property="og:title" content="HackingEDU"/>
-    <meta property="og:description" content="HackingEDU, the largest student run hackathon conceived with the intent to disrupt the classroom and push the envelope on the contemporary education system."/>
+    <meta property="og:description" content="HackingEDU, the largest education hackathon conceived with the intent to disrupt the classroom and push the envelope on the contemporary education system."/>
     <meta property="og:image" content="http://hackingedu.co/assets/img/hacklogo-prism.png"/>
 
     <title>HackingEDU</title>
