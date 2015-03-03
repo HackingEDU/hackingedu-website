3638e7cb58f611c10d00cc90dfa9f7c99363dc9c
James Kao <james@thekao.com>
2015-03-02 17:16:29 -0800

    rachel's website improvement suggestions

--- views/about.php
+++ views/about.php
@@ -3,11 +3,11 @@
             	<div class="col-lg-8">
                     <h1>What is <?php echo NAME; ?>?</h1>
                     <h3>About:</h3>
-                    <p>HackingEDU is a hackathon conceived and executed by students, for students, with the intent of disrupting the classroom and really pushing the envelope on the contemporary education system.  This is the largest student run hackathon encompassing all the major San Francisco Bay Area Universities and those along the west coast.</p>
+                    <p>HackingEDU is a hackathon conceived and executed by students, for students, with the intent of disrupting the classroom and really pushing the envelope on the contemporary education system.  This is the largest education hackathon encompassing all the major San Francisco Bay Area Universities and those along the west coast.</p>
                     <h3>Vision:</h3>
                     <p>A world where students believe they can take that big chance, that big dream, and make it a reality.</p>
                     <h3>Mission:</h3>
-                    <p>Our biggest goal is to inspire the next generation of students to solve the problems that plague our education system.  This is an opportunity for us as college students to raise our voice.  It’s our time to redefine education.  HackingEDU is excited to provide a launchpad for fellow students to invent the future.</p>
+                    <p>Our biggest goal is to inspire the next generation of students to solve the problems that plague our education system.  This is an opportunity for us, as college students, to raise our voice.  It’s our time to redefine education.  HackingEDU is excited to provide a launchpad for fellow students to invent the future.</p>
             	</div>
             	<div class="col-lg-4 hide-on-mobile">
             	     <span class="pull-right"><br><a class="apply-btn" href="#apply"><button type="button" class="btn btn-green">Sign Up Now!</button></a></span>
@@ -16,13 +16,13 @@
             	  	<!-- <h1>Our Process</h1> -->
             	  	<hr>
             	  	<h3><b>LEARN</b></h3>
-            	  	<p>We want the students to not only learn, but to show the world what new educational evolutions can be created to push our current system forward.  To inspire a new kind of educational system that works more effectively than the current one.</p>
+            	  	<p>We want students to not only learn, but to show the world what new educational evolutions can be created to push our current system forward.  To inspire a new kind of educational system that works more effectively than the current one.</p>
             	  	<br>
             	  	<!-- <h3><b>DESIGN</b></h3>
             	  	<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software.</p>
             	  	<br> -->
             	  	<h3><b>BUILD</b></h3>
-            	  	<p>It's time to show what you've got!  They say ideas are great, but execution is where people fall short.  Let's see what you can do in the 36 hours provided.</p>
+            	  	<p>It's time to show what we've got!  They say ideas are great, but execution is where people fall short.  Let's see what you can do in the 36 hours provided.</p>
             	  	<br>
             	  	<h3><b>EVOLVE</b></h3>
             	  	<p>We believe that the current education system is outdated.  If you are on the same wavelength as us then let's make a change.  Let us evolve the way we think about education.</p>
--- views/header.php
+++ views/header.php
@@ -9,7 +9,7 @@
     <meta property="og:url" content="http://hackingedu.co"/>
     <meta property="og:type" content="website"/>
     <meta property="og:title" content="HackingEDU"/>
-    <meta property="og:description" content="HackingEDU, the largest student run hackathon conceived with the intent to disrupt the classroom and push the envelope on the contemporary education system."/>
+    <meta property="og:description" content="HackingEDU, the largest education hackathon conceived with the intent to disrupt the classroom and push the envelope on the contemporary education system."/>
     <meta property="og:image" content="http://hackingedu.co/assets/img/hacklogo-prism.png"/>
     <link rel="shortcut icon" href="../assets/img/favicon.ico?v=1.1">
     <!-- <link rel="shortcut icon" href="hackinged.ico"> -->
